b70c0b7a884ca4f9d5972b06e29dd388e6b677c2
lucciii33 <angelomaiele@gmail.com>
2022-08-01 16:11:03 +0000

    not working

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
@@ -18,19 +19,23 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
+class StatusType(enum.Enum):
+    todo = "todo"
+    inProgress = "inProgress"
+    done = "done"
 class Project(db.Model):
+    
     id = db.Column(db.Integer, primary_key=True)
-    todo = db.Column(db.String(200), unique=False, nullable=True)
-    inProgress = db.Column(db.String(200), unique=False, nullable=True)
-    done = db.Column(db.String(200), unique=False, nullable=True)
+    task = db.Column(db.String(200), unique=False, nullable=True)
+    status_type = db.Column(db.Enum(StatusType),default=StatusType.todo, nullable=False)
+    
 
     def __repr__(self):
         return f'<User {self.todo}>'
 
     def serialize(self):
         return {
-            "todo": self.todo,
-            "inProgress": self.inProgress,
-            "done": self.done,
+            "task": self.task,
+            "status_type": self.status_type,
             # do not serialize the password, its a security breach
         }
