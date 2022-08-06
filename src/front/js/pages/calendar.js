afcfe766b1bc1bc00ca4c4ab6e2b36f39bdb8459
lucciii33 <angelomaiele@gmail.com>
2022-08-06 15:01:36 +0000

    calendar DOne

--- src/front/js/pages/calendar.js
+++ src/front/js/pages/calendar.js
@@ -1,29 +1,72 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
-import Calendar from 'react-calendar'
 import "../../styles/home.css";
-import 'react-calendar/dist/Calendar.css';
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US")
+}
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2022, 8, 0),
+        end: new Date(2022, 8, 0),
+    },
+    {
+        title: "Vacation",
+        start: new Date(2022, 8, 2),
+        end: new Date(2022, 8, 4),
+    },
+    {
+        title: "Conference",
+        start: new Date(2022, 8, 8),
+        end: new Date(2022, 8, 10),
+    },
+];
 
 export const Calendary = () => {
     const { store, actions } = useContext(Context);
-    const [calDate, setCalDate] = useState(new Date())
-    const [newDay, setNewDay] = useState([calDate]);
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
 
-    function onChange(calDate) {
-        console.log(calDate)
-        setCalDate(calDate)
+    function handleAddEvent() {
+        setAllEvents([...allEvents, newEvent]);
     }
-    console.log(newDay)
-    // function arrayToString(calDate) {
-    //     let arrayToStr = newDay.toString()
-    //     return arrayToStr
-    // }
 
     return (
+
         <div className="m-4 mt-5">
-            <Calendar className="cal" onChange={onChange} value={calDate} />
-            <h3>{calDate.toDateString()}</h3>
+            <h2>Add New Event</h2>
+            <div className="d-grid">
+                <input type="text" placeholder="Add Title" style={{ width: "20%" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} className="datepicker" />
+                <DatePicker placeholderText="Start Date" style={{}} selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} className="datepicker" />
+            </div>
+            <DatePicker placeholderText="End Date" selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} className="datepicker" />
+            <button onClick={handleAddEvent} className="btn-calendar">
+                Add Event
+            </button>
+            <Calendar localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+
+            <br />
+            <br />
         </div>
     );
 };
